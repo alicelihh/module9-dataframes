b920bb6bef94f6d0deb069104a25da292c010af2
Alice Li <alicehh@uw.edu>
2017-01-19 14:16:32 -0800

    Did some exercises

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -2,27 +2,28 @@
 
 # Create a vector of the number of points the Seahawks scored in each game this
 # season (google "Seahawks" for the info!)
-
+Seahawks <-c(30, 40, 50, 60, 70, 80)
 
 # Create a vector of the number of points the Seahwaks have allowed to be scored
 # against them in each game this season
-
+Opponent <- c(0, 1, 2, 3, 4, 5)
 
 # Combine your two vectors into a dataframe
-
+date <- data.frame(Seahawks, Opponent)
 
 # Create a new column "diff" that is the difference in points.
 # Hint: recall the syntax for assigning new elements (which in this case will be
 # a vector) to a list!
-
-
+diff <- c(Seahawks - Opponent)
 # Create a new column "won" which is TRUE if the Seahawks won
-
+won <- Seahawks > Opponent
 
 # Create a vector of the opponent names corresponding to the games played
-
+opp.names <- c("a", "b", "c", "d", "e", "f")
 
 # Assign your dataframe rownames of their opponents
+date <- data.frame(opp.names, Seahawks, Opponent,won)
 
 
 # View your data frame to see how it has changed!
+date
--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -1,38 +1,40 @@
 # Create a vector of 100 employees ("Employee 1", "Employee 2", ... "Employee 100)
 # Hint: use the `paste()` function to produce the list
-
+employees <- c(paste("Employee", (1:100)))
+employees
 
 # Create a vector of 100 random salaries for the year 2014
 # Use the `runif()` function to pick a random number between 40000 and 50000
-
+Salaries.2014 <- runif(100, 40000, 50000)
 
 # Create a vector of 100 salaries in 2015 that have increased from 2014 by some amount
 # Hint: use `runif()` to add a random number to 2014's salaries. Starting from a
 # _negative_ number so that salaries may decrease!
-
+Salaries.2015 <- Salaries.2014 + runif(100, -5000, 10000)
 
 # Create a data.frame 'salaries' by combining the 3 vectors you just made
 # Remember to set `stringsAsFactors=FALSE`!
-
-
+salaries <- data.frame(employees, Salaries.2014, Salaries.2015)
+View(salaries)
 # Create a column 'raise' that stores the size of the raise between 2014 and 2015
-
-
+salaries$raise <- salaries$Salaries.2015 - salaries$Salaries.2014
+View(salaries)
 # Create a column 'got.raise' that is TRUE if the person got a raise
-
-
+salaries$got.raised <- (salaries$raise > 0)
+View(salaries)
 ### Retrieve values from your data frame to answer the following questions
 ### Note that you should get the value as specific as possible (e.g., a single
 ### cell rather than the whole row!)
 
-# What was the 2015 salary of employee 57
 
+# What was the 2015 salary of employee 57
+salaries$Salaries.2015[57]
 
 # How many employees got a raise?
-
-
+count <- nrow(salaries[salaries$got.raised == TRUE, ])
+count
 # What was the value of the highest raise?
-
+highest.raise <- salaries[salaries$raise == max(salaries$raise), "employee"]
 
 # What was the "name" of the employee who received the highest raise?
 
